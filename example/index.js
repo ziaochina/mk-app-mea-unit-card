5e8fc9144dcc45353a2dba979788a3fc281c7837
ziaochina <ziaochina@gmail.com>
2017-10-11 17:40:57 +0800

    zlj

---
+++ example/index.js
@@ -0,0 +1,35 @@
+import { config, start, componentFactory } from 'mk-meta-engine'
+import * as mkComponents from 'mk-component'
+import myConfig  from './config'
+
+import mk_app_mea_unit_card_test from './apps/apps/mk-app-mea-unit-card-test/index.js'
+import mk_app_mea_unit_card from './apps/index.js'
+
+const apps = {
+		
+	[mk_app_mea_unit_card_test.name]: mk_app_mea_unit_card_test,	
+	[mk_app_mea_unit_card.name]: mk_app_mea_unit_card,
+}
+
+apps.config = (options) => {
+	Object.keys(options).forEach(key => {
+		const reg = new RegExp(`^${key == '*' ? '.*' : key}$`)
+		Object.keys(apps).forEach(appName => {
+			if (appName != 'config') {
+				if (reg.test(appName)) {
+					apps[appName].config(options[key])
+				}
+			}
+		})
+	})
+}
+
+apps.config({ '*': { apps } })
+
+config(myConfig({ apps }))
+
+Object.keys(mkComponents).forEach(key=>{
+	componentFactory.registerComponent(key, mkComponents[key])
+})
+	
+start()
---
+++ example/mock.js
@@ -0,0 +1,3 @@
+
+import './apps/apps/mk-app-mea-unit-card-test/mock.js';
+import './apps/mock.js';
