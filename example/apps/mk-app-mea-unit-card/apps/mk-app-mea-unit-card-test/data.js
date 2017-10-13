9be30b988057b7e335733539b918aebef454a46e
ziaochina <ziaochina@gmail.com>
2017-10-13 11:25:56 +0800

    zlj

--- example/apps/mk-app-mea-unit-card/apps/mk-app-mea-unit-card-test/data.js
+++ example/apps/mk-app-mea-unit-card/apps/mk-app-mea-unit-card-test/data.js
@@ -5,12 +5,12 @@ export function getMeta() {
 		children: [{
 			name:'add',
 			component:'Button',
-			children:'新增存货类型',
+			children:'新增计量单位',
 			onClick:'{{$add}}' 
 		},{
 			name:'modify',
 			component:'Button',
-			children:'修改存货类型',
+			children:'修改计量单位',
 			onClick:'{{$modify}}' 
 		}]
 	}
